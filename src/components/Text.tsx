baf65a7cea3ce3f0973947008ab45e2f5bd5f246
Diogo Cesar <d_cesarrodrigues@hotmail.com>
2022-10-13 22:39:42 -0300

    Página principal de login

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,5 +1,5 @@
-import { Slot } from '@radix-ui/react-slot'
-import { clsx } from 'clsx'
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
 import { ReactNode } from 'react';
 
 export interface TextProps {
